d78cec24919d16d7f4373f9c3882e20862c22ff8
Energon89 <podymov89@gmail.com>
2019-09-25 16:52:06 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-import './style.css';
-import './modules/main.js';
+import "./style.css";
+import "./modules/test2.js";
+import "./modules/test3.js";
+import "./modules/test4.js";
+import "./modules/test5.js";
+import "./modules/test6.js";
+import "./modules/test7.js";
+import "./modules/test8.js";
