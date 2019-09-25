ff224664b755a091e3955a4d7cc281836e1f03bf
Energon89 <podymov89@gmail.com>
2019-09-25 16:54:52 +0300

    uppdate

--- src/index.js
+++ src/index.js
@@ -1,8 +1,2 @@
 import "./style.css";
-import "./modules/test2.js";
-import "./modules/test3.js";
-import "./modules/test4.js";
-import "./modules/test5.js";
-import "./modules/test6.js";
-import "./modules/test7.js";
-import "./modules/test8.js";
+import "./modules/main.js";
---
+++ src/modules/main.js
@@ -0,0 +1,7 @@
+import "./test2.js";
+import "./test3.js";
+import "./test4.js";
+import "./test5.js";
+import "./test6.js";
+import "./test7.js";
+import "./test8.js";
